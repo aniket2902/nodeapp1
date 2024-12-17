dcf473b7975d4d6841b5a251cce9adb7a8035bb5
Aniket <aniketmahajan19@gmail.com>
2024-12-17 15:35:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send("App Version 1.0")
+})
+
+app.listen(4000, '0.0.0.0', ()=>{
+    console.log("Server started at port 4000")
+})
