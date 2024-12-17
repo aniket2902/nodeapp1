1977ba6d48205968e409a6565ce95a2ba8c73f7d
Aniket <aniketmahajan19@gmail.com>
2024-12-17 16:08:20 +0530

    version 2.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    response.send("App Version 1.0")
+    response.send("App Version 22.0")
 })
 
 app.listen(4000, '0.0.0.0', ()=>{
